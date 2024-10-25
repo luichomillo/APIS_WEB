40c27380f584e32d010de0c5af0991635b1f5a26
luichomillo <lvallejos120@gmail.com>
2024-10-25 15:59:19 -0300

    Update server.js
    agrego api Registrar Conexion

--- server.js
+++ server.js
@@ -50,3 +50,46 @@ process.on('SIGINT', () => {
     });
 });
 
+// *** Ruta para registrar la conexión de un usuario ***
+app.post('/api/register-connection', (req, res) => {
+    const { IP } = req.body;
+    console.log("REGISTRAR CONEXION (POST) DE LA IP: ", IP);
+    
+    let fecha = new Date();
+    let formattedDate = `${fecha.getFullYear()}-${(fecha.getMonth() + 1).toString().padStart(2, '0')}-${fecha.getDate().toString().padStart(2, '0')} ${fecha.getHours().toString().padStart(2, '0')}:${fecha.getMinutes().toString().padStart(2, '0')}`;
+
+    if (!IP) {
+        return res.status(400).json({ success: false, error: 'POST: IP no proporcionada' });
+    }
+
+    db.get('SELECT * FROM USUARIOS WHERE IP_USER = ?', [IP], (err, row) => {
+        if (err) {
+            console.log("ERROR EN EL SERVIDOR: ", err);
+            return res.status(500).json({ success: false, error: 'Error en el servidor' });
+        }
+
+        if (row) {
+            // Si ya existe un registro para esta IP, marcar como conectado
+            console.log("FECHA UPDATE", formattedDate);
+            db.run(`UPDATE USUARIOS SET VIVO = TRUE, Fecha_VIVO = ? WHERE IP_USER = ?`, [formattedDate, IP], function(err) {
+                if (err) {
+                    console.log("ERROR AL REGISTRAR CONEXION UPDATE: ", err);
+                    return res.status(500).json({ success: false, error: 'Error al actualizar la base de datos' });
+                }
+                console.log("REGISTRAR CONEXION OK");
+                return res.json({ success: true });
+            });
+        } else {
+            // Si la IP no existe, crear un nuevo usuario anónimo
+            console.log("FECHA INSERT: ", formattedDate);
+            db.run(`INSERT INTO USUARIOS (USER, IP_USER, VIVO, Fecha_VIVO) VALUES (?, ?, ?, ?)`, ['Anónimo', IP, true, formattedDate], function(err) {
+                if (err) {
+                    console.log("ERROR AL REGISTRAR CONEXION INSERT: ", err);
+                    return res.status(500).json({ success: false, error: 'Error al insertar en la base de datos' });
+                }
+                console.log("NUEVA IP");
+                return res.json({ success: true });
+            });
+        }
+    });
+});
