ce05a449d6c176872aca1813b13678b354f0049a
luichomillo <lvallejos120@gmail.com>
2024-10-30 21:08:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -458,8 +458,9 @@ app.post('/api/usermysql', (req, res) => {
         return res.status(400).json({ success: false, error: 'IP y USER son obligatorios' });
     }
 
-    const fechaVivo = new Date().toISOString().slice(0, 19).replace('T', ' '); // Obtener fecha y hora actual en formato 'YYYY-MM-DD HH:MM:SS'
-    
+    //const fechaVivo = new Date().toISOString().slice(0, 19).replace('T', ' '); // Obtener fecha y hora actual en formato 'YYYY-MM-DD HH:MM:SS'
+     const fechaVivo = new Date(new Date().getTime() - (3 * 60 * 60 * 1000)).toISOString().slice(0, 19).replace('T', ' '); // Ajustar a UTC-3
+	
     // Primero, intentamos actualizar al usuario existente
     const updateQuery = `UPDATE Usuarios SET USER = ?, VIVO = 1, FECHA_VIVO = ? WHERE IP_USER = ?`;
     mysqlConnection.query(updateQuery, [USER, fechaVivo, IP], (err, result) => {
