3b56a9c53a1b506dac30b65096497e1e2f47168e
luichomillo <lvallejos120@gmail.com>
2024-11-14 21:55:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -265,8 +265,10 @@ const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
     host: 'luichomillo.freeddns.org',
-    user: 'luichomillo',
-    password: 'Kuffour_70',
+    //user: 'luichomillo',
+    //password: 'Kuffour_70',
+    user: 'root',
+    password: '',
     database: 'luichomillo',
     port: 3306,
     connectTimeout: 10000 // Timeout de conexión de 10 segundos
